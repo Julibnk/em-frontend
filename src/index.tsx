1dc392f4523645499a81aef40def2a9a0618b60c
Julian <julian.fernandez@enzymeadvisinggroup.com>
2022-05-31 20:20:15 +0200

    Initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-const a = 2;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+process.env.REACT_APP_REPORT_WEB_VITALS && reportWebVitals(console.log);
