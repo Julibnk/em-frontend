164b1ae65dfae71b7744b7f626237e9e7a2795fd
Julian <julian.fernandez@enzymeadvisinggroup.com>
2022-05-31 20:12:45 +0200

    Enviroments

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log(process.env.REACT_APP_BACKEND_ENDPOINT);
   return (
     <div className="App">
       <header className="App-header">
